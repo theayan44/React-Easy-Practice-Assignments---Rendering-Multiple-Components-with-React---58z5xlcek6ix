1c0fcd00d23411b4b2958e5e395fc7251b9b0d3c
theayan44 <88241719+theayan44@users.noreply.github.com>
2022-04-24 11:56:11 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">ECommerce Website</h1>
+      <p data-ns-test="project-description">
+        This is a complete online platform where a user can do shopping
+      </p>
+    </div>
+  );
+};
 
 export default App;
